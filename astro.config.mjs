fe95ea374eb4279951ae5eec6bf784a7b59d810c
Florian Seidel <florian.seidel@huk-coburg.de>
2025-01-17 10:35:54 +0100

    first test

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,9 @@ import {defineConfig} from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  devToolbar: {
+    enabled: false,
+  },
   site: "https://pixelpalast.github.io",
   // base: "my-repo",
 });
